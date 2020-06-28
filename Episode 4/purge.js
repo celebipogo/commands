5bd518d2d941c4d674f73aeaf187f837b9e21216
BluD3V <65052071+BluD3V@users.noreply.github.com>
2020-06-28 12:57:53 -0500

    Add files via upload

--- Episode 4/purge.js
+++ Episode 4/purge.js
@@ -1,1 +1,21 @@
-test
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send("You are not allowed to run this command.");
+
+  const deleteCount = parseInt(args[0], 10);
+  if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+    return message.channel.send(
+      "Please specify how many messages you would like to purge. (min 2, max 100)"
+    );
+  message.channel
+    .bulkDelete(deleteCount)
+    .catch(error =>
+      message.channel.send(`Couldn't purge messages because of: ${error}.`)
+    );
+};
+
+module.exports.help = {
+  name: "purge"
+};
