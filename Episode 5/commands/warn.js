090d85df1115ca276c2728b313d32564ec99e283
BluD3V <65052071+BluD3V@users.noreply.github.com>
2020-06-29 13:36:43 -0500

    Add files via upload

---
+++ Episode 5/commands/warn.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+  if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send("You are not allowed to run this command.");
+
+  let wUser =
+    message.guild.member(message.mentions.users.first()) ||
+    message.guild.members.cache.get(args[0]);
+
+  if (!wUser) return message.channel.send("Please mention a valid user.");
+
+  if (wUser.hasPermission("MANAGE_MESSAGES"))
+    return message.channel.send("Unable to warn staff members.");
+
+  let reason = args.join(" ").slice(22);
+  if (!reason) reason = "No reason provided.";
+
+  if (!warns[wUser.id])
+    warns[wUser.id] = {
+      warns: 0
+    };
+
+  warns[wUser.id].warns++;
+
+  fs.writeFile("./warnings.json", JSON.stringify(warns), err => {
+    if (err) console.log(err);
+  });
+
+  let warnEmbed = new Discord.MessageEmbed()
+	.setTitle("Warning Logged")
+    .addField("User Warned", wUser)
+    .addField("Reason", reason)
+    .addField("Current Warnings", warns[wUser.id].warns)
+    .setColor("#48BCFF");
+
+  message.channel.send(warnEmbed);
+};
+
+module.exports.help = {
+  name: "warn"
+};
---
+++ Episode 5/commands/warnings.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+  let wUser =
+    message.guild.member(message.mentions.users.first()) ||
+    message.guild.members.cache.get(args[0]);
+
+  if (!wUser) return message.channel.send("Please mention a valid user.");
+
+  let warnl = warns[wUser.id].warns;
+  
+  let warnEmbed = new Discord.MessageEmbed()
+	.setTitle("Warnings")
+    .addField("User", wUser)
+    .addField("Total Warnings", warns[wUser.id].warns)
+    .setColor("#48BCFF");
+
+  message.channel.send(warnEmbed);
+};
+
+module.exports.help = {
+  name: "warnings"
+};
